4d5429386150d24759332eb510a8cf326ade4330
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-22 14:31:25 +0200

    add function to change the text on the button

--- js/script.js
+++ js/script.js
@@ -4,5 +4,10 @@ console.log(lamp, button);
 
 button.addEventListener('click',function(){
     lamp.src="../img/yellow_lamp.png";
+    changeButton();
     console.log("accesa")
 })
+
+function changeButton(){
+    button.innerText = "spegni";
+}
