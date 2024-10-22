b14dba9e2cdc5d2102568a63b0c74066c180edd4
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-22 16:07:46 +0200

    add if structure to switch the lamp

--- js/script.js
+++ js/script.js
@@ -11,11 +11,19 @@ const lamp = document.getElementById("lamp");
 const button = document.getElementById("switch");
 
 //imposto un flag per vedere se la lampadina è accesa o meno, come valore inizziale impostiamo falso perche è spenta
-const lampFlag = false;
+let lampFlag = false;
 
 //in base al flag scelgo cosa scrivere come valore inziale nel button (uso operatore ternario cioè abbreviazione dell'if)
 button.innerText = (!lampFlag) ? "Accendi" : "Spegni"; 
-//
-button.addEventListener('click',function(){})
+//qui tramite la funzione addeventlistener, al click facciamo partire una funzione che fa accedndere e spegnere la lampadina
+button.addEventListener('click',function(){
+    //controlliamo se la "lampadina è accesa"
+    if(lampFlag){//se lampadina è accesa, al click
+        lampFlag = false; //allora la spegniamo
+    }else{ // altriementi se è spenta, al click
+        lampFlag = true; //la accendiamo
+    }
+    
+})
 
 
