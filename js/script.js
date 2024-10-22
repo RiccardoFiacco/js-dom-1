788f0fc4686545aa9f31e02518b8b96d085d26ba
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-22 17:01:16 +0200

    add old solution

--- js/script.js
+++ js/script.js
@@ -28,3 +28,47 @@ button.addEventListener("click", function () {
     button.innerText = "Spegni"; //cambiamo la scritta del bottone in "Spegni"
   }
 });
+// function changeImgg(flag){
+//     if(flag===true){
+//         lamp.src="../img/yellow_lamp.png";
+//         console.log("sono nel ramo: 1");
+//         flag = false;
+
+// }else{
+//         lamp.src="../img/white_lamp.png";
+//         console.log("sono nel ramo: 2");
+//         flag =true;
+
+// }
+// return flag;
+// }
+// function changeButtonn(flag){
+//     if(flag===true){
+//         button.innerText = "spegni";
+//         console.log("sono nel ramo: 3");
+//         flag = false;
+
+// }else{
+//         button.innerText = "accendi";
+//         console.log("sono nel ramo: 4");
+//         flag =true;
+
+// }
+// return flag;
+// }
+
+
+// function changeImg() {
+// if (lamp.src === "http://127.0.0.1:5501/img/yellow_lamp.png") {
+// lamp.src = "../img/white_lamp.png";
+// } else {
+// lamp.src = "../img/yellow_lamp.png";
+// }
+// }
+// function changeButton() {
+// if (button.textContent === "accendi") {
+// button.innerText = "spegni";
+// } else {
+// button.innerText = "accendi";
+// }
+// }
