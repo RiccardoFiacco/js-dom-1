ea6c172ec799713de7ecd2272073a5295cfcbcd8
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-22 14:29:33 +0200

    add turn on the light with eventlistener

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 const lamp = document.getElementById("lamp");
 const button = document.getElementById("switch");
 console.log(lamp, button);
+
+button.addEventListener('click',function(){
+    lamp.src="../img/yellow_lamp.png";
+    console.log("accesa")
+})
