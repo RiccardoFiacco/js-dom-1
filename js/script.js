b67842a976de618b80d94558f1fe6b0faee55be5
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-22 15:04:53 +0200

    add a possible solution

--- js/script.js
+++ js/script.js
@@ -3,11 +3,49 @@ const button = document.getElementById("switch");
 console.log(lamp, button);
 
 button.addEventListener('click',function(){
-    lamp.src="../img/yellow_lamp.png";
-    changeButton();
-    console.log("accesa")
+    if(lamp.src ==="http://127.0.0.1:5501/img/yellow_lamp.png"){
+        lamp.src="../img/white_lamp.png";
+    }else{
+        lamp.src="../img/yellow_lamp.png";
+    }
+    if(button.textContent ==="accendi"){
+        button.innerText = "spegni"; 
+    }else{
+        button.innerText = "accendi"; 
+    }
 })
 
-function changeButton(){
-    button.innerText = "spegni";
-}
+// button.addEventListener('click',function(){ 
+//     let flag =true;
+//     console.log("stato flag prima click "+flag)
+//     changeImg(flag);
+//     console.log("stato flag durante click "+flag)
+//     flag = changeButton(flag);
+//     console.log("stato flag dopo click "+flag)
+// })
+
+// function changeImg(flag){
+//     if(flag===true){
+//         lamp.src="../img/yellow_lamp.png";
+//         console.log("sono nel ramo: 1");
+//         flag = false;
+//     }else{
+//         lamp.src="../img/white_lamp.png";
+//         console.log("sono nel ramo: 2");
+//         flag =true;
+//     }
+//     return flag;
+// }
+// function changeButton(flag){
+//     if(flag===true){
+//         button.innerText = "spegni";
+//         console.log("sono nel ramo: 3");
+//         flag = false;
+//     }else{
+//         button.innerText = "accendi";
+//         console.log("sono nel ramo: 4");
+//         flag =true;
+//     } 
+//     return flag;
+// }
+
