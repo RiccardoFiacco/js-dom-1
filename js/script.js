e0a0cbc1020a00be647fa8bef3cb54773b93aaa9
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-22 15:56:55 +0200

    add ternary operator

--- js/script.js
+++ js/script.js
@@ -13,8 +13,9 @@ const button = document.getElementById("switch");
 //imposto un flag per vedere se la lampadina è accesa o meno, come valore inizziale impostiamo falso perche è spenta
 const lampFlag = false;
 
-button.addEventListener('click',function(){
-        
-})
+//in base al flag scelgo cosa scrivere come valore inziale nel button (uso operatore ternario cioè abbreviazione dell'if)
+button.innerText = (!lampFlag) ? "Accendi" : "Spegni"; 
+//
+button.addEventListener('click',function(){})
 
 
